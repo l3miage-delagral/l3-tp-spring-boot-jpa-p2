b4bd4b484c3bbbda8eaa0efa041f04cd6adac717
l3miage-delagral <alex.del57640@gmail.com>
2023-03-09 16:04:08 +0100

    push

--- l3-tp-spring-boot-jpa-main/data/src/main/java/fr/uga/l3miage/library/data/domain/Librarian.java
+++ l3-tp-spring-boot-jpa-main/data/src/main/java/fr/uga/l3miage/library/data/domain/Librarian.java
@@ -4,6 +4,7 @@ import java.util.Objects;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.DiscriminatorValue;
+import jakarta.persistence.Entity;
 
 @Entity
 @DiscriminatorValue(value = "librarian")
