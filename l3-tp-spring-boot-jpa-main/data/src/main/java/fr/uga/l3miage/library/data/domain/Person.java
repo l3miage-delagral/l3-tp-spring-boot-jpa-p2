3fa58eaf7a5d42ec84211d486c4bdefcd30dc4ff
l3miage-chazeauv <vincent.chazeau05@gmail.com>
2023-03-09 16:03:09 +0100

    là pas d'erreur

--- l3-tp-spring-boot-jpa-main/data/src/main/java/fr/uga/l3miage/library/data/domain/Person.java
+++ l3-tp-spring-boot-jpa-main/data/src/main/java/fr/uga/l3miage/library/data/domain/Person.java
@@ -5,11 +5,13 @@ import java.util.Objects;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.DiscriminatorColumn;
+import jakarta.persistence.DiscriminatorType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Inheritance;
+import jakarta.persistence.InheritanceType;
 import jakarta.persistence.Temporal;
 import jakarta.persistence.TemporalType;
 
